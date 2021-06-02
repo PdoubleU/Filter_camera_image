a87221cfc60ee22577a2505937d8da3ea42dea1d
PdoubleU <piotrwitasik1988@gmail.com>
2021-06-02 22:17:22 +0200

    [get video]: application is accesing user's webcam and geting stream of data [paint to canvas]: programm is rendering captured data from webcam [image edges]: this function will be converting captured video into image with edges (very popular filter) 	- now this function is creating the temporary copy of image and looping tru original image for each pixel 	- later on function will be converting each pixel regarding to the values of all 9 neighbouring pixels to make some calculations

--- index.js
+++ index.js
@@ -3,3 +3,75 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+        .then(localMediaStream => {
+            video.srcObject = localMediaStream;
+            video.play();
+        })
+        .catch(err =>
+            console.log("Error", err)
+        )
+}
+
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.height = height;
+    canvas.width = width;
+
+        ctx.drawImage(video, 0, 0, width, height);
+        let pixels = ctx.getImageData(0, 0, width, height);
+        pixels = imageEdges(pixels);
+        ctx.putImageData(pixels, 0, 0);
+
+    // return setInterval(() => {
+    //     ctx.drawImage(video, 0, 0, width, height);
+    //     let pixels = ctx.getImageData(0, 0, width, height);
+    //     pixels = imageEdges(pixels);
+    //     ctx.putImageData(pixels, 0, 0);
+    // }, 20);
+}
+
+function imageEdges(pixels) {
+    console.log(pixels);
+    let tmpPixels = [];
+    let returnedImageData;
+    let width = pixels.width;
+    let height = pixels.height;
+
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+            // pixels.data[((row * (width * 4)) + (col * 4)) + 0] = pixels.data[((row * (width * 4)) + (col * 4)) + 0] + 200;
+            // pixels.data[((row * (width * 4)) + (col * 4)) + 1] = pixels.data[((row * (width * 4)) + (col * 4)) + 1] + 50;
+            // pixels.data[((row * (width * 4)) + (col * 4)) + 2] = pixels.data[((row * (width * 4)) + (col * 4)) + 2] + 160;
+            // pixels.data[((row * (width * 4)) + (col * 4)) + 2] = pixels.data[((row * (width * 4)) + (col * 4)) + 2] + 160;
+            r = pixels.data[((row * (width * 4)) + (col * 4)) + 0] + 200;
+            g = pixels.data[((row * (width * 4)) + (col * 4)) + 1] + 50;
+            b = pixels.data[((row * (width * 4)) + (col * 4)) + 2] + 150;
+            a = pixels.data[((row * (width * 4)) + (col * 4)) + 3] + 0;
+            tmpPixels.push(r, g, b, a);
+        }
+    }
+
+    tmpPixels = Uint8ClampedArray.from(tmpPixels);
+    returnedImageData = new ImageData(tmpPixels, width, height);
+
+    return returnedImageData;
+}
+
+function rgbSplit(pixels) {;
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i - 150] = pixels.data[i + 0];
+        pixels.data[i + 500] = pixels.data[i + 1];
+        pixels.data[i - 300] = pixels.data[i + 2];
+    }
+
+    return pixels;
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
